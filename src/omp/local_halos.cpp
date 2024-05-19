ac81342caa99dd2c939459b577437759197f7faf
Daniele Piano <dpiano01@gmail.com>
2024-05-19 21:23:22 +0200

    [TOPOLOGY] aligning OMP local_halos.cpp

--- src/omp/local_halos.cpp
+++ src/omp/local_halos.cpp
@@ -65,50 +65,53 @@ void update_bottom(const int x, const int y, const int halo_depth, const int dep
 }
 
 // Updates faces in turn.
-void update_face(const int x, const int y, const int halo_depth, const int *chunk_neighbours, const int depth, double *buffer,
-                 bool is_offload) {
-  if (chunk_neighbours[CHUNK_LEFT] == EXTERNAL_FACE) {
+void update_face(const int x, const int y, const int halo_depth, const int depth, double *buffer, bool is_offload) {
+  int neighbours_rank[NUM_DIRECTION_NEIGHBOURS];
+
+  get_cart_neighbours_rank(X_AXIS, 1, neighbours_rank);
+  if (neighbours_rank[LEFT] == MPI_PROC_NULL) {
     update_left(x, y, halo_depth, depth, buffer, is_offload);
   }
-  if (chunk_neighbours[CHUNK_RIGHT] == EXTERNAL_FACE) {
+  if (neighbours_rank[RIGHT] == MPI_PROC_NULL) {
     update_right(x, y, halo_depth, depth, buffer, is_offload);
   }
-  if (chunk_neighbours[CHUNK_TOP] == EXTERNAL_FACE) {
+
+  get_cart_neighbours_rank(Y_AXIS, 1, neighbours_rank);
+  if (neighbours_rank[UP] == MPI_PROC_NULL) {
     update_top(x, y, halo_depth, depth, buffer, is_offload);
   }
-  if (chunk_neighbours[CHUNK_BOTTOM] == EXTERNAL_FACE) {
+  if (neighbours_rank[DOWN] == MPI_PROC_NULL) {
     update_bottom(x, y, halo_depth, depth, buffer, is_offload);
   }
 }
 
 // The kernel for updating halos locally
-void local_halos(const int x, const int y, const int depth, const int halo_depth, const int *chunk_neighbours,
-                 const bool *fields_to_exchange, double *density, double *energy0, double *energy, double *u, double *p, double *sd,
-                 bool is_offload) {
+void local_halos(const int x, const int y, const int depth, const int halo_depth, const bool *fields_to_exchange, double *density,
+                 double *energy0, double *energy, double *u, double *p, double *sd, bool is_offload) {
   if (fields_to_exchange[FIELD_DENSITY]) {
-    update_face(x, y, halo_depth, chunk_neighbours, depth, density, is_offload);
+    update_face(x, y, halo_depth, depth, density, is_offload);
   }
   if (fields_to_exchange[FIELD_P]) {
-    update_face(x, y, halo_depth, chunk_neighbours, depth, p, is_offload);
+    update_face(x, y, halo_depth, depth, p, is_offload);
   }
   if (fields_to_exchange[FIELD_ENERGY0]) {
-    update_face(x, y, halo_depth, chunk_neighbours, depth, energy0, is_offload);
+    update_face(x, y, halo_depth, depth, energy0, is_offload);
   }
   if (fields_to_exchange[FIELD_ENERGY1]) {
-    update_face(x, y, halo_depth, chunk_neighbours, depth, energy, is_offload);
+    update_face(x, y, halo_depth, depth, energy, is_offload);
   }
   if (fields_to_exchange[FIELD_U]) {
-    update_face(x, y, halo_depth, chunk_neighbours, depth, u, is_offload);
+    update_face(x, y, halo_depth, depth, u, is_offload);
   }
   if (fields_to_exchange[FIELD_SD]) {
-    update_face(x, y, halo_depth, chunk_neighbours, depth, sd, is_offload);
+    update_face(x, y, halo_depth, depth, sd, is_offload);
   }
 }
 
 // Solver-wide kernels
 void run_local_halos(Chunk *chunk, Settings &settings, int depth) {
   START_PROFILING(settings.kernel_profile);
-  local_halos(chunk->x, chunk->y, depth, settings.halo_depth, chunk->neighbours, settings.fields_to_exchange, chunk->density,
-              chunk->energy0, chunk->energy, chunk->u, chunk->p, chunk->sd, settings.is_offload);
+  local_halos(chunk->x, chunk->y, depth, settings.halo_depth, settings.fields_to_exchange, chunk->density, chunk->energy0, chunk->energy,
+              chunk->u, chunk->p, chunk->sd, settings.is_offload);
   STOP_PROFILING(settings.kernel_profile, __func__);
 }
